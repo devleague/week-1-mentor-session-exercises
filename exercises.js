32647d0eed22b8ae56f883deec3cee136c059a4f
Steven Inouye <steveninouye@msn.com>
2017-12-15 00:33:47 -1000

    first commit

--- exercises.js
+++ exercises.js
@@ -5,7 +5,13 @@
  * @return {string} reversed
  * ie: "cat" => "tac"
  */
-
+function firstReverse(str){
+  if (typeof str === 'string') {
+    return str.split('').reverse().join('')
+  } else {
+    return null;
+  }
+}
  /** Function: alphaOrder
  * The function will take the str parameter being passed in and
  * return the string in alphabetical order
@@ -13,7 +19,13 @@
  * @return {string} in alphabetical order
  * ie: "cake" => "acek"
  */
-
+function alphaOrder(str){
+  if (typeof str === 'string'){
+    return str.split('').sort().join('');
+  } else {
+    return null;
+  }
+}
  /** Function: vowelCount
  * The function will take the str parameter being passed in and
  * return the number of vowels in the string
@@ -21,7 +33,17 @@
  * @return {number} count of vowels
  * ie: "oreo" => 3
  */
-
+function vowelCount(str){
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  if(typeof str === 'string'){
+    return str.split('').reduce((acc, curr) => {
+      (vowels.indexOf(curr.toLowerCase()) !== -1) ? acc++ : acc;
+      return acc;
+    }, 0)
+  } else {
+    return null;
+  }
+}
  /** Function: timeConvert
  * The function will take the str parameter representing the amount of minutes being passed in and
  * return the number of hours and minutes. Seperate the number of hours
@@ -30,7 +52,13 @@
  * @return {string} as hours:minutes
  * ie: 68 => 1:8
  */
-
+function timeConvert(str){
+  if(typeof str === 'number'){
+    return `${(str / 60).toFixed(0)}:${str % 60}`;
+  } else {
+    return null;
+  }
+}
  /** Function: repeatString
  * The function will take in two parameters and repeat a given string (first argument)
  * num times (second argument). Return an empty string if num is a negative number
@@ -39,13 +67,23 @@
  * @return {string} repeated num times
  * i.e repeatString("money", 3) => "moneymoneymoney".
  */
-
+function repeatString(str, times){
+  if(typeof str === 'string' && typeof times === 'number'){
+    if(times > 0){
+      return str.repeat(times);
+    } else {
+      return '';
+    }
+  } else {
+    return null;
+  }
+}
 
 /**
  * Below here we see a module.exports which is set to an object with a bunch of keys.
  * The module.exports syntax is a built-in javascript keyword that
- * allows functionality from this file to be used in other files.  
- * Here we are exporting an object with a bunch of keys that will reference 
+ * allows functionality from this file to be used in other files.
+ * Here we are exporting an object with a bunch of keys that will reference
  * the functions you have made. After you are done implementing a function,
  * change the 'null' to reference the corresponding function you have just created.
  * Then go into your terminal and run 'npm test' to see if the tests pass for your function.
@@ -56,9 +94,9 @@
  */
 
 module.exports = {
-    firstReverse: null,
-    alphaOrder: null,
-    vowelCount: null,
-    timeConvert: null,
-    repeatString: null
+    firstReverse: firstReverse,
+    alphaOrder: alphaOrder,
+    vowelCount: vowelCount,
+    timeConvert: timeConvert,
+    repeatString: repeatString
 }
